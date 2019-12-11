f879d574176a68c764291e67905b0ff58913c457
Knaoinr <knaoinr@gmail.com>
2019-12-11 17:20:33 -0500

    Initial Commit

---
+++ word by word/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  word by word
+//
+//  Created by Kiera O'Flynn on 12/11/19.
+//  Copyright © 2019 Knaoinr. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
